dc78522933304620d04d6642cf2e4ea82bfcdf53
eliooreuxx <luhaida13172@gmail.com>
2021-11-24 15:16:24 +0100

    test and DV

--- DV/src/pkc.rs
+++ DV/src/pkc.rs
@@ -31,6 +31,6 @@ pub fn encrypt(pkr: &[u8], msg: &[u8], ad: &[u8]) -> Vec<u8> {
 pub fn decrypt(sk: &[u8], ct: &[u8], ad: &[u8]) -> &[u8] {
     let sk_key = RSAPrivateKey::from_pkcs1(sk).unwrap();
     let msg = sk_key.decrypt(PaddingScheme::new_pss(ad), &ct).expect("failed to decrypt");
-    ms
+    msg.as_slice()
 }
 
